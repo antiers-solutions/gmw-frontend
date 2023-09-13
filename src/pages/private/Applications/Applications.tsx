e652bc35254be53116b840e572e9040c0450c502
harshveer <harshveer.singh@antiersolutions.com>
2023-09-13 18:12:48 +0530

    design: Application, Delivery main page.

--- src/pages/private/Applications/Applications.tsx
+++ src/pages/private/Applications/Applications.tsx
@@ -1,7 +1,60 @@
+import { Table } from "react-bootstrap";
 import "./Applications.scss";
+import Check from "../../../assets/svg/check.svg";
+import msgIcon from "../../../assets/svg/msgicon.svg";
+import { useNavigate } from "react-router-dom";
+import { dummyObject } from "../../../helper/dummy";
 
 const Applications = () => {
-  return <div>Coming Soon</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="applicationsSec">
+      <div className="heading">
+        <h3>Applications</h3>
+      </div>
+      <div className="aplicationTop">
+        <Table className={` common_table  aplicationTable `}>
+          <thead>
+            <tr>
+              <th>
+                <button className="opnBtn">Open</button>
+                <button className="closeBtn">Close</button>
+              </th>
+              <th>Reviews</th>
+              <th>Assignee</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {dummyObject.map((item, idx) => (
+              <tr  onClick={() => {
+                navigate(`/auth/projects/`);
+              }}>
+                <td key={idx}>
+                  <p>
+                    {item.name}
+                    <img src={Check} />
+                  </p>
+                  <p>{item.review}</p>
+                </td>
+                <td>
+                  <img src={msgIcon} /> {item.count}
+                </td>
+                <td>
+                  <div className="multiImgsDiv">
+                    {item.image.map((items: string, index) => (
+                      <img key={index} src={items} className="multiImg" />
+                    ))}
+                  </div>
+                </td>
+                
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default Applications;
--- src/pages/private/Deliveries/Deliveries.tsx
+++ src/pages/private/Deliveries/Deliveries.tsx
@@ -1,11 +1,55 @@
-import "./Deliveries.scss";
+import { Table } from "react-bootstrap";
+import "../Applications/Applications.scss";
+import Check from "../../../assets/svg/check.svg";
+import msgIcon from "../../../assets/svg/msgicon.svg";
+import { dummyObject } from "../../../helper/dummy";
 
-const Deliveries = () => {
+const Applications = () => {
   return (
-    <div className="inner-layout">
-      <span>Coming Soon</span>
+    <div className="applicationsSec">
+      <div className="heading">
+        <h3>Deliveries</h3>
+      </div>
+      <div className="aplicationTop">
+        <Table className={` common_table  aplicationTable `}>
+          <thead>
+            <tr>
+              <th>
+                <button className="opnBtn">Open</button>
+                <button className="closeBtn">Close</button>
+              </th>
+              <th>Reviews</th>
+              <th>Assignee</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {dummyObject.map((item, idx) => (
+              <tr>
+                <td key={idx}>
+                  <p>
+                    {item.name}
+                    <img src={Check} />
+                  </p>
+                  <p>{item.review}</p>
+                </td>
+                <td>
+                  <div className="multiImgsDiv">
+                    {item.image.map((items: string, index) => (
+                      <img key={index} src={items} className="multiImg" />
+                    ))}
+                  </div>
+                </td>
+                <td>
+                  <img src={msgIcon} /> {item.count}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 };
 
-export default Deliveries;
+export default Applications;
